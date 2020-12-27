4c0bb43de756f7c66075ab0ecc5d5536e99f5bb6
oogioogi <detacl73@gmail.com>
2020-12-27 18:42:32 +0900

    Initial Commit

---
+++ ChatMessage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatMessage
+//
+//  Created by 이용석 on 2020/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
